f0b29e569594d689ade13de17eb456aa481da93f
ElMehdi Tahri <elmehdi.tahri@orange.com>
2020-03-30 19:32:59 +0100

    change vendor name

--- index.php
+++ index.php
@@ -24,11 +24,11 @@ $casesByCountry = $controller->getCasesByCountry();
   <title>COVID 19 - Global Statistics</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="dist/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="dist/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -191,24 +191,24 @@ $casesByCountry = $controller->getCasesByCountry();
 
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="dist/jquery/jquery.min.js"></script>
+  <script src="dist/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="dist/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="dist/js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="dist/chart.js/Chart.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-  <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <script src="dist/datatables/jquery.dataTables.min.js"></script>
+  <script src="dist/datatables/dataTables.bootstrap4.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="js/demo/datatables-demo.js"></script>
+  <script src="dist/js/demo/datatables-demo.js"></script>
 
   
 </body>
